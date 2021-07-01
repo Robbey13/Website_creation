a497eb62c4167975b26bd315c41786fa93fa3adf
Robbey13 <86802239+Robbey13@users.noreply.github.com>
2021-07-01 15:27:15 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,10 @@
+
+function spanelem() {
+    window.alert("this is a SPAN element");
+}
+
+function divelem() {
+    window.alert("this is a DIV element")
+
+    document.getElementById("audio").play();
+}
